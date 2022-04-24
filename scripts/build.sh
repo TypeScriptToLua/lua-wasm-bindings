69f4f757efe593c615faecbbf776a713a9379740
Lorenz Junglas <lolleko@users.noreply.github.com>
2022-04-24 21:31:14 +0200

    Fix removing WASM from linker args instead of compiler args

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,13 +6,13 @@ mkdir -p `dirname "$0"`/../dist/glue
 cd `dirname "$0"`/../thirdparty/$1
 
 make clean
-make generic MYLIBS= MYCFLAGS= CC='emcc -O3 -s WASM=1' AR='emar rcu' RANLIB='emranlib'
+make generic MYLIBS= MYCFLAGS= CC='emcc -O3' AR='emar rcu' RANLIB='emranlib'
 
 cd ../..
 
 if [[ "$1" == "lua-5.1.5" ]]; then
     emcc -Ithirdparty/$1 thirdparty/$1/src/liblua.a \
-        -O3 -o dist/glue/glue-$1.js \
+        -s WASM=1 -O3 -o dist/glue/glue-$1.js \
         -s EXPORTED_RUNTIME_METHODS="['cwrap']" \
         -s MODULARIZE=1 \
         -s ALLOW_TABLE_GROWTH \
@@ -36,7 +36,7 @@ if [[ "$1" == "lua-5.1.5" ]]; then
     ]"
 else
     emcc -Ithirdparty/$1 thirdparty/$1/src/liblua.a \
-        -O3 -o dist/glue/glue-$1.js \
+        -s WASM=1 -O3 -o dist/glue/glue-$1.js \
         -s EXPORTED_RUNTIME_METHODS="['cwrap']" \
         -s MODULARIZE=1 \
         -s ALLOW_TABLE_GROWTH \
