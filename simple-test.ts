e73b0377e32e3254b0102806a2070e41ebccd6ae
Lorenz Junglas <lolleko@users.noreply.github.com>
2022-04-25 16:08:43 +0200

    Added simple test
    to check if all functions that are needed are exported correctly

---
+++ simple-test.ts
@@ -0,0 +1,15 @@
+import * as Lua50 from "./dist/lua.51"
+
+Lua50.lauxlib.luaL_newstate();
+
+import * as Lua51 from "./dist/lua.52"
+
+Lua51.lauxlib.luaL_newstate();
+
+import * as Lua52 from "./dist/lua.53"
+
+Lua52.lauxlib.luaL_newstate();
+
+import * as Lua53 from "./dist/lua.54"
+
+Lua53.lauxlib.luaL_newstate();
