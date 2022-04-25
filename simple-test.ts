0b89cd659888295e48d80228b32e750a9da10ee4
Lorenz Junglas <lolleko@users.noreply.github.com>
2022-04-25 18:38:53 +0200

    Improve test

--- simple-test.ts
+++ simple-test.ts
@@ -1,19 +1,18 @@
+import { LauxLib, Lua, LuaLib } from "./dist/lua";
+
 import * as Lua50 from "./dist/lua.50";
-
-Lua50.lauxlib.luaL_newstate();
-
 import * as Lua51 from "./dist/lua.51";
-
-Lua51.lauxlib.luaL_newstate();
-
 import * as Lua52 from "./dist/lua.52";
-
-Lua52.lauxlib.luaL_newstate();
-
 import * as Lua53 from "./dist/lua.53";
-
-Lua53.lauxlib.luaL_newstate();
-
 import * as Lua54 from "./dist/lua.54";
 
-Lua54.lauxlib.luaL_newstate();
+function simpleTest(luaBundle: {lua: Lua, lauxlib: LauxLib, lualib: LuaLib}) {
+    const state = luaBundle.lauxlib.luaL_newstate();
+    luaBundle.lualib.luaL_openlibs(state);
+}
+
+simpleTest(Lua50);
+simpleTest(Lua51);
+simpleTest(Lua52);
+simpleTest(Lua53);
+simpleTest(Lua54);
